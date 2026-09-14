1749862f465fe62b5623a3acf282be945a4ed912
Emi <dillingerstaffing@users.noreply.github.com>
2026-09-14 05:57:15 +0000

    add call-or-text phone option to contact groups

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // VERSION: bump this string on every deploy that changes cached assets, so clients install a fresh service worker.
-const VERSION = 'v20260913-1755';
+const VERSION = 'v20260914-0156';
 const CACHE = 'tapeout-' + VERSION;
 const OFFLINE_URL = 'index.html';
 

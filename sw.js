956d8147010b6e4200b5923ea151281b24299fb1
dillingerstaffing <dillingerstaffing@users.noreply.github.com>
2026-09-15 10:57:14 +0000

    Refresh build version after Ghostlight link added to footer

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // VERSION: bump this string on every deploy that changes cached assets, so clients install a fresh service worker.
-const VERSION = 'v20260915-tapeout-0625';
+const VERSION = 'v20260915-tapeout-0655';
 const CACHE = 'tapeout-' + VERSION;
 const OFFLINE_URL = 'index.html';
 
